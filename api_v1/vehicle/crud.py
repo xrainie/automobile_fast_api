6be21900b3f727e0fa5acc0c058d7cfd3e014a16
xoxrainie <xoxrainie@gmail.com>
2024-08-06 07:59:16 +0300

    create vehicle and get_vehicles done

--- api_v1/vehicle/crud.py
+++ api_v1/vehicle/crud.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
-from .schemas import VehicleQueryParamsSchema, VehicleSchema
+from sqlalchemy.engine import Result
+from .schemas import VehicleQueryParamsSchema, VehicleSchema, CreateVehicleSchema
 from core.models import VehicleModel
 from fastapi import Depends, HTTPException, status
 from typing import Annotated
@@ -10,28 +11,33 @@ attrs = ["brand", "model", "year_of_issue", "fuel", "gearbox", "mileage", "min_p
 
 
 async def get_vehicles(session: AsyncSession, params: Annotated[VehicleQueryParamsSchema, Depends()]):
-    paramsdict = params.model_dump(exclude_unset=True)
+    paramsdict = params.model_dump(exclude_none=True, exclude_unset=True)
+    print(paramsdict)
     stmt = select(VehicleModel)
     if 'brand' in paramsdict:
         stmt = stmt.where(VehicleModel.brand == paramsdict['brand'])
     if 'model' in paramsdict:
-        stmt = stmt.where(VehicleModel.model == paramsdict['brand'])
+        stmt = stmt.where(VehicleModel.model == paramsdict['model'])
     if 'year_of_issue' in paramsdict:
-        stmt = stmt.where(VehicleModel.year_of_issue == paramsdict['brand'])
+        stmt = stmt.where(VehicleModel.year_of_issue == paramsdict['year_of_issue'])
     if 'fuel' in paramsdict:
-        stmt = stmt.where(VehicleModel.fuel == paramsdict['brand'])
+        stmt = stmt.where(VehicleModel.fuel == paramsdict['fuel'])
     if 'gearbox' in paramsdict:
-        stmt = stmt.where(VehicleModel.gearbox == paramsdict['brand'])
+        stmt = stmt.where(VehicleModel.gearbox == paramsdict['gearbox'])
     if 'mileage' in paramsdict:
-        stmt = stmt.where(VehicleModel.mileage == paramsdict['brand'])
+        stmt = stmt.where(VehicleModel.mileage == paramsdict['mileage'])
     if 'min_price' in paramsdict:
         stmt = stmt.where(VehicleModel.price >= paramsdict['min_price'])
     if 'max_price' in paramsdict:
         stmt = stmt.where(VehicleModel.price <= paramsdict['max_price'])
                 
-    result = await session.execute(stmt)
+    result: Result = await session.execute(stmt)
+    vehicles = result.scalars().all()
+    return list(vehicles)
 
-    if result is not None:
-        return result
-    
-    return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='По заданным фильтрам ничего не найдено.')
+
+async def create_vehicle(session: AsyncSession, vehicle: CreateVehicleSchema) -> VehicleSchema:
+    vehicle_inst = VehicleModel(**vehicle.model_dump())
+    session.add(vehicle_inst)
+    await session.commit()
+    return vehicle_inst
