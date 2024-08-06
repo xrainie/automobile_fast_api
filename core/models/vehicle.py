9f4a9a1c687309288f1d96266e9fa9a146d5c72d
xoxrainie <xoxrainie@gmail.com>
2024-08-06 07:57:42 +0300

    fix vehicle model

--- core/models/vehicle.py
+++ core/models/vehicle.py
@@ -5,8 +5,8 @@ from sqlalchemy.orm import Mapped
 class VehicleModel(BaseModel):
     brand: Mapped[str]
     model: Mapped[str]
-    year_of_issue = Mapped[int]
-    fuel = Mapped[str]
-    gearbox = Mapped[str]
-    mileage = Mapped[int]
-    price = Mapped[int]
+    year_of_issue: Mapped[int]
+    fuel:Mapped[str]
+    gearbox: Mapped[str]
+    mileage: Mapped[int]
+    price: Mapped[int]
