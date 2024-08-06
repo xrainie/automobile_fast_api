4140c666d341746c1c6116dd18025e7be79903fd
xoxrainie <xoxrainie@gmail.com>
2024-08-06 03:08:55 +0300

    add Vehicle Model

--- core/models/vehicle.py
+++ core/models/vehicle.py
@@ -1,1 +1,12 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped
+from .base import BaseModel
+from sqlalchemy.orm import Mapped
+
+
+class Vehicle(BaseModel):
+    brand: Mapped[str]
+    model: Mapped[str]
+    year_of_issue = Mapped[int]
+    fuel = Mapped[str]
+    gearbox = Mapped[str]
+    mileage = Mapped[int]
+    price = Mapped[int]
