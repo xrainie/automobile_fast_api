dade0fbdc7d0eb86c92329364331ca6ea4db0ffa
xoxrainie <xoxrainie@gmail.com>
2024-08-06 07:59:32 +0300

    views done

--- api_v1/vehicle/views.py
+++ api_v1/vehicle/views.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from typing import Annotated
+from typing import Annotated, List
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api_v1.vehicle.schemas import VehicleQueryParamsSchema
@@ -7,12 +7,17 @@ from api_v1.vehicle.schemas import VehicleQueryParamsSchema
 from . import crud
 
 from core.db_helper import get_db
-from .schemas import VehicleSchema
+from .schemas import VehicleSchema, CreateVehicleSchema
 
 
 router = APIRouter(tags=['vehicle'])
 
 
-@router.get('/', response_model=VehicleSchema)
-async def get_vehicles(session: Annotated[AsyncSession, Depends(get_db)], params: Annotated[VehicleQueryParamsSchema, Depends()]):
-    await crud.get_vehicles(session=session, params=params)
+@router.get('/', response_model=list[VehicleSchema])
+async def get_vehicles(session: Annotated[AsyncSession, Depends(get_db)], params: Annotated[VehicleQueryParamsSchema, Depends()]) -> list[VehicleSchema]:
+    return await crud.get_vehicles(session=session, params=params)
+
+
+@router.post('/', response_model=VehicleSchema)
+async def create_vehicle(session: Annotated[AsyncSession, Depends(get_db)], vehicle: CreateVehicleSchema):
+   return await crud.create_vehicle(session=session, vehicle=vehicle)
